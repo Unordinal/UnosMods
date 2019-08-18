b4205cf22fbe96d0e949bb89f0704ad24832feff
Unordinal <11812639+Unordinal@users.noreply.github.com>
2019-08-17 19:27:38 -0700

    Attempting to make UI building easier.

---
+++ UnosUtilities/UIHelper/UIHelper.cs
@@ -0,0 +1,37 @@
+﻿using RoR2.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnosUtilities.UIHelper
+{
+    public static class UIHelper
+    {
+        public static GameObject BuildCanvasBackground(string name = "UIHelper.UIBase", Vector2 ctrSizeDelta = new Vector2(), bool usesCursor = false)
+        {
+            GameObject uiBase = new GameObject
+            {
+                name = name,
+                layer = 5
+            };
+            var canvas = uiBase.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            canvas.sortingOrder = -1;
+            uiBase.AddComponent<GraphicRaycaster>();
+            uiBase.AddComponent<MPEventSystemProvider>().fallBackToMainEventSystem = true;
+            uiBase.AddComponent<MPEventSystemLocator>();
+            if (usesCursor)
+                uiBase.AddComponent<CursorOpener>();
+
+            GameObject uiCtr = new GameObject
+            {
+                name = "UIHelper.UIContainer",
+            };
+            uiCtr.transform.SetParent(uiBase.transform, false);
+            uiCtr.AddComponent<RectTransform>().sizeDelta = new Vector2(0.5f, 0.5f);
+            return uiBase;
+        }
+    }
+}
