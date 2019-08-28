6d27f0ddaecb864e6632a2edd6029efd42215d73
Unordinal <11812639+Unordinal@users.noreply.github.com>
2019-08-27 19:59:07 -0700

    MiscBalanceTweaks - Adds/changes various things about the game. Currently lowers min skill cooldown to a configurable value which makes things like Brainstalks better.

---
+++ MiscBalanceTweaks/MiscBalanceTweaks.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using MonoMod.Cil;
+using static UnosMods.MiscBalanceTweaks.MiscBalanceTweaksConfig;
+
+namespace UnosMods.MiscBalanceTweaks
+{
+    [BepInDependency(R2API.R2API.PluginGUID)]
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+
+    public class MiscBalanceTweaks : BaseUnityPlugin
+    {
+        public const string PluginGUID = "com.unordinal.MiscBalanceTweaks";
+        public const string PluginName = "MiscBalanceTweaks";
+        public const string PluginVersion = "1.0.0";
+        internal new static ManualLogSource Logger { get; } = new ManualLogSource(PluginName);
+
+        public MiscBalanceTweaks()
+        {
+            Init(Config);
+            if (MinSkillCooldown != 0.5f)
+            {
+                Logger.LogInfo("Creating hook.");
+                IL.RoR2.GenericSkill.RecalculateFinalRechargeInterval += GenericSkill_RecalculateFinalRechargeInterval;
+            }
+            else
+                Logger.LogWarning("MinSkillCooldown is 0.5, which is the vanilla value. Did you intend to change this?");
+        }
+
+        private void GenericSkill_RecalculateFinalRechargeInterval(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            c.GotoNext(
+                x => x.MatchLdcR4(0.5f),
+                x => x.MatchLdarg(0),
+                x => x.MatchLdfld(out _),
+                x => x.MatchLdarg(0),
+                x => x.MatchCallvirt(out _)
+            );
+            c.Next.Operand = MinSkillCooldown;
+        }
+    }
+}
---
+++ MiscBalanceTweaks/MiscBalanceTweaksConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using BepInEx.Configuration;
+
+namespace UnosMods.MiscBalanceTweaks
+{
+    public static class MiscBalanceTweaksConfig
+    {
+        private static ConfigWrapper<float> minSkillCooldown;
+        public static float MinSkillCooldown { get => Mathf.Clamp(minSkillCooldown.Value, 0f, 2f); }
+        public static void Init(ConfigFile cfg)
+        {
+            minSkillCooldown = cfg.Wrap(
+                "BalanceTweaks",
+                "MinSkillCooldown",
+                "Sets the minimum skill cooldown. (Default: 0.1f, Vanilla: 0.5f)",
+                0.1f
+                );
+        }
+
+        public static KeyCode? GetKey(ConfigWrapper<string> param)
+        {
+            if (!Enum.TryParse(param.Value, out KeyCode result))
+                return null;
+            return result;
+        }
+    }
+}
