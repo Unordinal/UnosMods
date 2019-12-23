2484e9a3002df94a59f95a1c083975616c6d3352
Unordinal <11812639+Unordinal@users.noreply.github.com>
2019-12-22 20:29:07 -0800

    Fixed 'Both Bands' modifier.

--- TabItemStats/AbstractModifiers/BothBands.cs
+++ TabItemStats/AbstractModifiers/BothBands.cs
@@ -7,7 +7,10 @@ namespace UnosMods.TabItemStats.AbstractModifiers
     public sealed class BothBands : AbstractModifier
     {
         protected override Func<float, float> Func =>
-            result => 1 - Mathf.Pow(1 - result, (ContextProvider.ItemStacks(RoR2.ItemIndex.IceRing) + ContextProvider.ItemStacks(RoR2.ItemIndex.FireRing)));
+            result => 1 - Mathf.Pow(1 - result, 
+                ((ContextProvider.ItemStacks(RoR2.ItemIndex.IceRing) > 0 ? 1 : 0) + 
+                (ContextProvider.ItemStacks(RoR2.ItemIndex.FireRing) > 0 ? 1 : 0)
+                ));
         protected override IStatFormatter Formatter => new ModifierFormatter("Both Bands");
     }
 }
