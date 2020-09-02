3a333383cc235aa0cae0e8960039307dec635eb5
Unordinal <unordinal@gmail.com>
2020-09-02 13:21:36 -0700

    Random utility stuff. Not meant to be used as a plugin.

---
+++ UnoUtilities/Networking/TestMessage.cs
@@ -0,0 +1,50 @@
+﻿using R2API.Networking.Interfaces;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+namespace Unordinal.UnoUtilities.Networking
+{
+    internal struct TestMessage : INetMessage
+    {
+        public string TestMessageStr { get; set; }
+        public List<int> TestMessageIntList { get; set; }
+
+        public void Serialize(NetworkWriter writer)
+        {
+            writer.Write(TestMessageStr);
+            writer.Write(TestMessageIntList.Count);
+            foreach (var item in TestMessageIntList)
+            {
+                writer.Write(item);
+            }
+
+            UnoUtilities.Logger.LogDebug($"Serialized {nameof(TestMessage)}.");
+            LogContents();
+        }
+
+        public void Deserialize(NetworkReader reader)
+        {
+            TestMessageStr = reader.ReadString();
+            TestMessageIntList = new List<int>();
+            foreach (var item in TestMessageIntList)
+            {
+                TestMessageIntList.Add(reader.ReadInt32());
+            }
+
+            UnoUtilities.Logger.LogDebug($"Deserialized {nameof(TestMessage)}.");
+            LogContents();
+        }
+
+        public void OnReceived()
+        {
+            UnoUtilities.Logger.LogDebug($"Received {nameof(TestMessage)}.");
+            LogContents();
+        }
+
+        private void LogContents()
+        {
+            UnoUtilities.Logger.LogDebug($"{nameof(TestMessageStr)}: {TestMessageStr}");
+            UnoUtilities.Logger.LogDebug($"{nameof(TestMessageIntList)} ({TestMessageIntList.Count}): {string.Join(", ", TestMessageIntList)}");
+        }
+    }
+}
---
+++ UnoUtilities/UnoUtilities.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using R2API.Networking;
+using R2API.Utils;
+using System.Collections.Generic;
+using UnityEngine;
+using Unordinal.UnoUtilities.Networking;
+
+namespace Unordinal.UnoUtilities
+{
+    [BepInDependency(R2API.R2API.PluginGUID)]
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync)]
+    public class UnoUtilities : BaseUnityPlugin
+    {
+        public const string PluginGUID = "Unordinal.UnoUtilities";
+        public const string PluginName = "UnoUtilities";
+        public const string PluginVersion = "1.0.0";
+        public static new ManualLogSource Logger { get; private set; }
+
+
+        // Called when the script instance is initialized. Only called once during the lifetime of the script.
+        internal void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogError("This should not be loaded as a plugin! WHATTERUDOIN?");
+            Destroy(this);
+        }
+
+        /// <summary>
+        /// Gets the currently loaded plugins. Dictionary keys are the mods' GUIDs.
+        /// </summary>
+        /// <returns></returns>
+        public static Dictionary<string, PluginInfo> GetLoadedPlugins()
+        {
+            return BepInEx.Bootstrap.Chainloader.PluginInfos;
+        }
+
+        public static BaseUnityPlugin GetPluginInstance(string pluginGUID)
+        {
+            BaseUnityPlugin instance = null;
+
+            var loadedPlugins = GetLoadedPlugins();
+            if (loadedPlugins.ContainsKey(pluginGUID))
+            {
+                instance = loadedPlugins[pluginGUID].Instance;
+            }
+
+            return instance;
+        }
+    }
+}
